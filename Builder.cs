d11a367639884b0f686c597a72f9bbc2835f7e48
replacewythy <replacewythy@hotmail.com>
2013-05-16 02:56:30 -0400

    Added TargetCompile

--- Builder.cs
+++ Builder.cs
@@ -7,6 +7,8 @@ namespace Builder
     public class Builder
     {
         private const string buildDir = "build";
+        private const string outputFile = "build/WebCreatures.exe";
+        private static string files = "";
 
         static void Main(string[] args)
         {
@@ -39,18 +41,44 @@ namespace Builder
         {
             if(Directory.Exists(buildDir))
             {
-                Directory.Delete(buildDir);
+                Directory.Delete(buildDir, true);
             }
         }
 
         public static void TargetCompile()
         {
-            
+            TargetClean();
+            Directory.CreateDirectory(buildDir);
+            DirectoryInfo thisDir = new DirectoryInfo(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location));
+            string currentPath = thisDir.ToString();
+            DirectoryInfo[] srcArray = thisDir.GetDirectories("src");
+            if(srcArray.Length == 1)
+            {
+                DirectoryInfo srcDir = srcArray[0];
+                FileInfo[] sourceFiles = srcDir.GetFiles("*.cs", SearchOption.AllDirectories);
+                foreach(FileInfo fi in sourceFiles)
+                {
+                    addFile(fi, currentPath);
+                }
+                Process process = new Process();
+                process.StartInfo.FileName = "mcs";
+                process.StartInfo.Arguments = files.Trim() + " -out:" + outputFile;
+                process.Start();
+            }
+            else
+            {
+                Console.WriteLine("There is a problem with the src directory.");
+            }
         }
 
         public static void TargetTest()
         {
-            
+            TargetCompile();
+        }
+
+        public static void addFile(FileInfo fi, string path)
+        {
+            files += " " + fi.ToString();
         }
     }
 }
